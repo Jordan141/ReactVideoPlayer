189e6a568ba1b69e3ac9efe0ed10707ea824e4a3
Jordan Moss <jmoss636@gmail.com>
2017-04-09 21:47:31 +0100

    API key removal

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
-const API_KEY = 'AIzaSyC3-pZFSeNmXF_q6ijDU1sObRD35zFLreI'
+const API_KEY = 'API KEY HERE'
 
 // Create a new component. This component should produce some HTML
 class App extends Component {
